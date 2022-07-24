0c2d0b2bb156d73baaa81c542f0f8a0c2ea09359
Jagi <ba87688@gmail.com>
2022-07-24 11:08:58 -0500

    suspending function

--- app/src/main/java/com/example2/roomapp/data/UserDao.kt
+++ app/src/main/java/com/example2/roomapp/data/UserDao.kt
@@ -10,7 +10,7 @@ import androidx.room.Query
 interface UserDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun addUser(user: User)
+    suspend fun addUser(user: User)
 
     @Query("SELECT * FROM user_table ORDER BY id ASC")
     fun readAllData(): LiveData<List<User>>
