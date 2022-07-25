5529575f40acf0ca3fc3a612b7a6cedd52f80d3f
Jagi <ba87688@gmail.com>
2022-07-25 03:33:23 -0500

    viewbinding added.

--- app/src/main/java/com/example2/roomapp/MainActivity.kt
+++ app/src/main/java/com/example2/roomapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example2.roomapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example2.roomapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.tvMainview.text = "Layout inflated"
+        setContentView(binding.root)
     }
 }
