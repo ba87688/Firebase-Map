faa9b62967d346f794fe39c0ef1d4068d1beef71
Jagi <ba87688@gmail.com>
2022-07-25 11:02:17 -0500

    database working

--- app/src/main/java/com/example2/roomapp/fragments/main/RemindersFragment.kt
+++ app/src/main/java/com/example2/roomapp/fragments/main/RemindersFragment.kt
@@ -1,20 +1,43 @@
 package com.example2.roomapp.fragments.main
 
+import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
 import com.example2.roomapp.R
+import com.example2.roomapp.data.Reminder
+import com.example2.roomapp.data.database.RemindersDatabase
+import com.example2.roomapp.databinding.FragmentRemindersBinding
+import kotlinx.coroutines.launch
 
 class RemindersFragment : Fragment() {
+    private var _binding: FragmentRemindersBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reminders, container, false)
+        _binding= FragmentRemindersBinding.inflate(inflater,container,false)
+
+
+
+
+        val db = Room.databaseBuilder(activity?.applicationContext!!,RemindersDatabase::class.java,"reminders_database").allowMainThreadQueries().build()
+
+        lifecycleScope.launch {
+            db.reminderDao().insertReminder(Reminder("Hi","desceibe","er","33","rr","t"))
+            val r = db.reminderDao().getReminderById("t")
+            Log.i("TAG", "onCreateView: $r")
+        }
+
+        return binding.root
     }
 
 
