349f6f1ed264a00c4d23dfb5a51bf2a6dbbdf6ed
Jagi <ba87688@gmail.com>
2022-08-05 02:45:04 -0500

    loginviewmodel test added

---
+++ app/src/test/java/com/example2/roomapp/viewmodels/login/LoginViewModelTest.kt
@@ -0,0 +1,29 @@
+package com.example2.roomapp.viewmodels.login
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginViewModelTest {
+
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+//    @Test
+//    fun addNewTask_setsNewTaskEvent() {
+//
+//        // Given a fresh ViewModel
+//        val tasksViewModel = LoginViewModel(,ApplicationProvider.getApplicationContext())
+//
+//        // When adding a new task
+//        tasksViewModel.restaurants
+//
+//        // Then the new task event is triggered
+//        // TODO test LiveData
+//    }
+}
