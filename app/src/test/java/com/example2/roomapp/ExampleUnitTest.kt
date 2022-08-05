f3ea4c6a400c6fe396f4604cb7ac91e74fb80f06
Jagi <ba87688@gmail.com>
2022-08-05 01:42:53 -0500

    tests update

--- app/src/test/java/com/example2/roomapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example2/roomapp/ExampleUnitTest.kt
@@ -14,4 +14,12 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    @Test
+    fun addition_isCorrect1() {
+        assertEquals(5, 2 + 2)
+    }
+    @Test
+    fun addition_isCorrect3() {
+        assertEquals(6, 2 + 2)
+    }
 }
