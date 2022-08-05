3413bf6aaee1692ca24d14caf7fed3a39e7b5c75
Jagi <ba87688@gmail.com>
2022-08-05 03:42:53 -0500

    small updatd to viewmodel test

--- app/src/test/java/com/example2/roomapp/viewmodels/login/LoginViewModelTest.kt
+++ app/src/test/java/com/example2/roomapp/viewmodels/login/LoginViewModelTest.kt
@@ -1,29 +1,41 @@
 package com.example2.roomapp.viewmodels.login
 
+import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example2.roomapp.data.RemainderDao
+import com.example2.roomapp.data.database.RemindersDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LoginViewModelTest {
+    private lateinit var database: RemindersDatabase
+    private lateinit var dao: RemainderDao
 
+
+    @Before
+    fun setup(){
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).allowMainThreadQueries().build()
+        dao = database.reminderDao()
+
+    }
+    @After
+    fun teardown(){
+        database.close()
+    }
 
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-//    @Test
-//    fun addNewTask_setsNewTaskEvent() {
-//
-//        // Given a fresh ViewModel
-//        val tasksViewModel = LoginViewModel(,ApplicationProvider.getApplicationContext())
-//
-//        // When adding a new task
-//        tasksViewModel.restaurants
-//
-//        // Then the new task event is triggered
-//        // TODO test LiveData
-//    }
+
 }
